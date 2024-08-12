dbf03e5ddc073ca002d7f0cbd551a16859ace2d3
asc2563 <allistersaintclair@outlook.com>
2024-08-11 22:06:46 -0700

    modified package.json and App.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,36 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  let city = prompt("What city do you want to check the weather for?");
-  if (city = null || city == "") {
-    city = "Seattle";
-    alert("You didn't enter a city, so we will use Seattle instead");
-  } else {
-    city = city;
-  }
   const [data, setData] = React.useState(null);
-
+  const [city, setCity] = React.useState('');
   React.useEffect(() => {
-    fetch(`https://api.codetabs.com/v1/weather?city=${city}`)
-      .then((res) => res.json())
-      .then((data) => setData(data));
-      console.log(data);
-  }, []);
+    const fetchData = async () => {
+      try {
+        let response = await fetch(`https://api.codetabs.com/v1/weather?city=${city}`);
+        let result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
+    };
+
+    if (city) {
+      fetchData();
+    }
+  }, [city]);
+
+  const handleCityChange = () => {
+    let newCity = prompt("What city do you want to check the weather for?");
+    if (newCity) {
+      localStorage.city = newCity;
+      setCity(newCity);
+    }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={handleCityChange}>set City</button>
         <p>temperature fahrenheit: {!data ? "Loading..." : data.tempF}</p>
         <br></br>
         <p>temperature celsius: {!data ? "Loading..." : data.tempC}</p>
